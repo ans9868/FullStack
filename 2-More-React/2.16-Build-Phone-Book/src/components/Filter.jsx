4e3a4073c54437157f2b6e903c347b6e14ac7f01
Nour999 <130527901+ans9868@users.noreply.github.com>
2024-03-25 12:53:42 -0400

    adding 2.16-Build

---
+++ 2-More-React/2.16-Build-Phone-Book/src/components/Filter.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import personServices from '../services/persons'
+
+
+const Filter = ({persons, filter, setPersons }) => {
+
+    console.log("persons")
+    console.log(persons)
+    const personsToShow = filter.length === 0
+        ? persons
+        : persons.filter(person => person.name.slice(0,filter.length).toLowerCase() === filter.toLowerCase())
+
+    const onDelete = (id) => {
+        console.log(id)
+        personServices.onDelete(id)
+            .then( () => {
+
+                setPersons(prevPersons => prevPersons.filter(person => person.id !== id))
+            })
+            .catch(error => {
+                console.error("error deleting person:", error)
+            })
+    }
+
+    return (
+        <ul>
+            {personsToShow.map(person =>
+                <ul key={person.name}>
+                    {person.name} {person.number}
+                    <button onClick={() => onDelete(person.id)}>Delete</button>
+                </ul>
+            )}
+        </ul>
+    )
+}
+
+export default Filter
---
+++ 2-More-React/2.16-Build-Phone-Book/src/services/persons.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+//const baseUrl = 'http://localhost:3001/persons' //the original
+//const baseUrl = 'http://localhost:3001/api/persons' //blocked by coors
+const baseUrl = '/api/persons' // this only added so it works for the production build when the /dist folder (when ready for distribution) is moved into the backend folder , essentailly since the backend/front end are in the same folder they can access each other locally
+//app.use(express.static('dist')) //need to add this to the backend so that the frontend can access the path of the api relatively (like above), if the path is not done relatively it is blocked by the CORS security system (since the bakcend/frontend are not in the same path it can create security vunerabilities)
+
+
+/*
+If want to remove coors problem id allow for requests from all origins add the code below. Should not be for deployment, only for testing
+const cors = require('cors')
+
+app.use(cors())
+ */
+
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+
+const create = (newObject) => {
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => response.data)
+}
+
+const onDelete = (id) => {
+    console.log("in delete person")
+    const request = axios.delete(`${baseUrl}/${id}`) // anxios . delete takes the full url
+    console.log("deleted person")
+    return request.then(response => response.data)
+}
+
+
+export default { getAll, create, onDelete }
