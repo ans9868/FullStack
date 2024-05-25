c91915f809751f10335bd48a2be49c3cb413d1d5
Nour999 <130527901+ans9868@users.noreply.github.com>
2024-05-25 14:19:40 -0700

    5.17 finished

--- 5-Frontend/5.13-Blog-List-Frontend/.test.js
+++ 5-Frontend/5.13-Blog-List-Frontend/.test.js
@@ -1,31 +1,31 @@
-import { afterEach } from 'vitest'
-import { cleanup, render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/vitest'
-
-import Blog from './src/components/Blog.jsx'
-
-afterEach(() => { //after each test, cleanup is executed
-    cleanup()
-})
-
-test('renders content', () => {
-    const blog = {
-        title: 'testTitle',
-        author: 'testAuthor',
-        url: 'testURL',
-        likes: 111
-    }
-
-    render(<Blog blog={blog} handleDelete={() => {}} handleAddLike={() => {}}/> )
-
-    const element = screen.getByClassName('aBlog')
-    expect(element).toBeDefined();
-})
+// import { afterEach } from 'vitest'
+// import { cleanup, render, screen } from '@testing-library/react'
+// import '@testing-library/jest-dom/vitest'
+//
+// import Blog from './src/components/Blog.jsx'
+//
+// afterEach(() => { //after each test, cleanup is executed
+//     cleanup()
+// })
+//
+// test('renders content', () => {
+//     const blog = {
+//         title: 'testTitle',
+//         author: 'testAuthor',
+//         url: 'testURL',
+//         likes: 111
+//     }
+//
+//     render(<Blog blog={blog} handleDelete={() => {}} handleAddLike={() => {}}/> )
+//
+//     const element = screen.getByClassName('aBlog')
+//     expect(element).toBeDefined();
+// })
 
 
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Blog from './src/components/Blog.jsx';
+import Blog from './src/components/Blog.jsx'
 
 test('simple render test', () => {
     const blog = {
