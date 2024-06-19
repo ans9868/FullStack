0553b7875e3c4fac7e1caac3eb23b21f473ab2af
Nour999 <130527901+ans9868@users.noreply.github.com>
2024-06-18 22:06:39 -0700

    finished 6.14

---
+++ 6-Flux-Arch/6.14-Flux/src/components/AnecdoteList.jsx
@@ -0,0 +1,40 @@
+import { voteAnecdote } from '../reducers/anecdoteReducer.js'
+import { postNotification} from "../reducers/notificationReducer.js";
+import { useSelector, useDispatch } from 'react-redux'
+
+const AnecdoteList = () => {
+    // const anecdotes = useSelector(state => state.anecdotes)
+    const anecdotes = useSelector(state => {
+        // console.log(state.anecdotes)
+        // console.log(state.filter)
+        return state.anecdotes.filter(dote => dote.content.includes(state.filter))
+    })
+    const dispatch = useDispatch()
+
+    const vote = (id) => {
+        // console.log('vote button pressed: ', id)
+        const content = anecdotes.find(n => n.id === id).content
+        dispatch(voteAnecdote(id))
+        dispatch(postNotification(`you voted '${content}'`))
+    }
+
+    return (
+        <div>
+            {[...anecdotes]
+            .sort((a, b) => b.votes - a.votes)
+            .map(anecdote =>
+                <div key={anecdote.id}>
+                    <div>
+                        {anecdote.content}
+                    </div>
+                    <div>
+                        has {anecdote.votes}
+                        <button onClick={() => vote(anecdote.id)}>vote</button>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default AnecdoteList
