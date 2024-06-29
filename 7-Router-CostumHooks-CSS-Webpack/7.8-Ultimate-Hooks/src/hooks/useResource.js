f9cd47ef6a4bae5cf6f61e1299f03262d988cdc9
Nour999 <130527901+ans9868@users.noreply.github.com>
2024-06-29 09:52:15 -0700

    finished 7.8

---
+++ 7-Router-CostumHooks-CSS-Webpack/7.8-Ultimate-Hooks/src/hooks/useResource.js
@@ -0,0 +1,39 @@
+import {useState, useEffect} from "react";
+import axios from "axios";
+
+export const useResource = (baseUrl) => {
+    const [resources, setResources] = useState([])
+
+    const getAll = () => {
+        axios.get(baseUrl)
+            .then(r => {
+            setResources(r.data)
+            })
+            .catch(error => {
+                console.log(`Error caught in create: ${error}`)
+            })
+    }
+
+    const create = async (resource) => {
+        try {
+            const r = await axios.post(baseUrl, resource)
+            setResources(resources.concat(resource))
+            return r.data
+        } catch (error) {
+            console.error(`Error creating resource ${error}`)
+            throw error
+        }
+    }
+
+    const service = {
+        create
+    }
+
+    useEffect(() => {
+        getAll()
+    }, []); //empty dependency, only run on mount
+
+    return [
+        resources, service
+    ]
+}
