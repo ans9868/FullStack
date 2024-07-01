5c8dba0932eb7d7121ff1a9ddadb9711da6dba56
Nour999 <130527901+ans9868@users.noreply.github.com>
2024-07-01 15:58:52 -0700

    finished 7.9

---
+++ 7-Router-CostumHooks-CSS-Webpack/7.9-Prettier-Code-Formatting/frontend/src/components/Notification.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types'
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return <div className={'error'}>{message}</div>
+}
+
+Notification.PropTypes = {
+  message: PropTypes.string.isRequired,
+}
+
+export default Notification
