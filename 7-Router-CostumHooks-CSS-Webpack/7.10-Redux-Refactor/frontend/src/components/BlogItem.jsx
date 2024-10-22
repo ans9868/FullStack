4744e512557830e68e715514580b593cff88b310
Nour999 <130527901+ans9868@users.noreply.github.com>
2024-10-21 22:54:43 -0400

    finish 17.17

--- 7-Router-CostumHooks-CSS-Webpack/7.10-Redux-Refactor/frontend/src/components/BlogItem.jsx
+++ 7-Router-CostumHooks-CSS-Webpack/7.10-Redux-Refactor/frontend/src/components/BlogItem.jsx
@@ -7,34 +7,9 @@ const BlogItem = ({ blog, handleAddLike, handleDelete }) => {
   const blogRef = useRef()
   const [localLikes, setLocalLikes] = useState(blog.likes)
 
-  const handleLocalLike = () => {
-    setLocalLikes(localLikes + 1) // Update local UI for likes
-    handleAddLike(blog) // Dispatch the like action to Redux
-
-    if (blogRef.current) {
-      blogRef.current.show()
-    } else {
-      console.error('blogRef.current is null or undefined')
-    }
-  }
-
-  const username = useSelector((state) => state.authentication.user.username)
-
   return (
     <div data-testid="aBlogPost" key={blog.id}>
       <Link to={`/blogs/${blog.id}`}> {blog.title} </Link>
-      <div data-testid="postAuthor"> {blog.author} </div>
-      <Togglable buttonLabel="view" ref={blogRef}>
-        {blog.url}
-        <br />
-        <div data-testid="postLikes"> Likes: {localLikes} </div>
-        <button onClick={handleLocalLike}> Like!</button>
-        <br />
-        {blog.user.name} <br />
-        {username === blog.user.username ? (
-          <button onClick={() => handleDelete(blog)}>Delete</button>
-        ) : null}
-      </Togglable>
     </div>
   )
 }
